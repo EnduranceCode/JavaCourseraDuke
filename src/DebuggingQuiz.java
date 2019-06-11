0566f1842e727ca541e70015d8966ea1b5c18209
EnduranceCode <endurancecode@gmail.com>
2019-06-11 13:03:54 +0100

    Fix the index out of bounds exception

--- src/DebuggingQuiz.java
+++ src/DebuggingQuiz.java
@@ -3,7 +3,7 @@ public class DebuggingQuiz {
 	public void findAbc(String input) {
 	    int index = input.indexOf("abc");
 	    while (true) {
-	        if (index == -1) {
+	        if (index == -1 || index >= input.length() - 3) {
 	            break;
 	        }
 	        String found = input.substring(index+1, index+4);
