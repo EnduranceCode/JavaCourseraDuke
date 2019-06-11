159c264b43de90d5350140c5e957cde88636d440
EnduranceCode <endurancecode@gmail.com>
2019-06-11 12:52:29 +0100

    Insert the given code

---
+++ src/DebuggingQuiz.java
@@ -0,0 +1,22 @@
+
+public class DebuggingQuiz {
+	public void findAbc(String input) {
+	    int index = input.indexOf("abc");
+	    while (true) {
+	        if (index == -1) {
+	            break;
+	        }
+	        String found = input.substring(index+1, index+4);
+	        System.out.println(found);
+	        index = input.indexOf("abc", index+4);
+	    }
+	}
+	   public void test() {
+		   findAbc("abcd");
+	}
+	public static void main(String[] args) {
+		DebuggingQuiz debuggingQuiz = new DebuggingQuiz();
+		debuggingQuiz.test();
+	}
+
+}
