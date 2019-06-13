c722dc4b6c606bd8a80f27e03b1c7a10266fa4d3
EnduranceCode <endurancecode@gmail.com>
2019-06-13 16:43:53 +0100

    Complete Part 3 of the assignment

--- src/ParsingExportData.java
+++ src/ParsingExportData.java
@@ -14,6 +14,7 @@ public class ParsingExportData {
 	String countryInfo (CSVParser parser, String country) {
 		
 		for (CSVRecord record : parser) {
+			
 			String countryName = record.get("Country");
 			
 			if (countryName.contains(country)) {
@@ -24,12 +25,38 @@ public class ParsingExportData {
 		return "NOT FOUND";
 	}
 	
+	/**
+	 * This method prints the names of all the countries that have both exportItem1 and exportItem2 as export items
+	 * 
+	 * @param	parser
+	 * @param	exportItem1
+	 * @param	exportItem2
+	 */
+	public void listExportersTwoProducts (CSVParser parser, String exportItem1, String exportItem2) {
+		
+		for (CSVRecord record : parser) {
+			
+			String exports = record.get("Exports");
+			
+			if (exports.contains(exportItem1) && exports.contains(exportItem2)) {
+				System.out.println(record.get("Country"));
+			}
+		}
+	}
+	
 	public void tester() {
 		FileResource fileResource = new FileResource("exports_small.csv");
 		CSVParser parser = fileResource.getCSVParser();
 		
+		System.out.println("CountryInfo Output:");
 		System.out.println(countryInfo(parser, "Germany"));
+		System.out.println("\n");
 		
+		parser = fileResource.getCSVParser();
+
+		System.out.println("listExportersTwoProducts:");
+		listExportersTwoProducts(parser, "gold", "diamonds");
+		System.out.println("\n");
 	}
 
 	public static void main(String[] args) {
